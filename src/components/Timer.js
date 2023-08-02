b4b562e61ac1b33e7a99baf36f63b441d652b4a5
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-08-02 15:28:35 -0700

    Create timer component

---
+++ src/components/Timer.js
@@ -0,0 +1,145 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Button from './Button';
+
+const Timer = () => {
+  const [time, setTime] = useState(5 * 60); // Default 5 minutes in seconds
+  const [isRunning, setIsRunning] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [inputTime, setInputTime] = useState(5); // User input for time
+  const [selectedUnit, setSelectedUnit] = useState("minutes"); // Default unit: minutes
+
+  const audioRef = useRef(null); // Ref for audio element
+
+  // Timer logic
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning && time > 0) {
+      intervalId = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (time === 0) {
+      // Timer reached 0, stop it and play sound
+      setIsRunning(false);
+      handleSoundAlert();
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning, time]);
+
+  // Format time in hh:mm:ss
+  const formatTime = (timeInSeconds) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+    const formattedHours = hours.toString().padStart(2, "0");
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = seconds.toString().padStart(2, "0");
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  // Handle start/stop button click
+  const handleStartStop = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+    setShowForm(false); // Hide the form when starting the timer
+  };
+
+  // Handle reset button click
+  const handleReset = () => {
+    if (isRunning) {
+      setIsRunning(false);
+    }
+
+    setShowForm(true);
+  };
+
+  // Handle sound alert
+  const handleSoundAlert = () => {
+    if (audioRef.current) {
+      audioRef.current.play(); // Start the audio playback
+    }
+  };
+
+  // Handle stop sound
+  const handleStopSound = () => {
+    if (audioRef.current) {
+      audioRef.current.pause(); // Pause the audio playback
+      audioRef.current.currentTime = 0; // Reset audio time to start
+    }
+  };
+
+  // Handle input change for custom timer
+  const handleInputChange = (e) => {
+    setInputTime(e.target.value);
+  };
+
+  // Handle select change for unit of time (seconds, minutes, hours)
+  const handleSelectChange = (e) => {
+    setSelectedUnit(e.target.value);
+  };
+
+  // Handle setting custom timer
+  const handleSetCustomTimer = () => {
+    const unitInSeconds = {
+      seconds: 1,
+      minutes: 60,
+      hours: 3600,
+    };
+    setTime(inputTime * unitInSeconds[selectedUnit]);
+    setShowForm(false);
+  };
+
+  return (
+    <div className="timer">
+      {showForm ? (
+        <div className="timer-form">
+          <label htmlFor="timerInput">Set Timer (minutes):</label>
+          <input
+            id="timerInput"
+            type="number"
+            min="1"
+            value={inputTime}
+            onChange={handleInputChange}
+          />
+          <select value={selectedUnit} onChange={handleSelectChange}>
+            <option value="seconds">Seconds</option>
+            <option value="minutes">Minutes</option>
+            <option value="hours">Hours</option>
+          </select>
+          <Button onClick={handleSetCustomTimer} text="Set" />
+        </div>
+      ) : (
+        <div className="timer-display">
+          {time === 0 ? "Time is up!" : formatTime(time)}
+        </div>
+      )}
+      <div className="timer-controls">
+        {time === 0 ? (
+          <>
+            <Button onClick={handleStopSound} text="Ok" />
+            <Button onClick={handleReset} text="Reset" />
+          </>
+        ) : (
+          <>
+            {isRunning ? (
+              <>
+                <Button onClick={handleStartStop} text="Stop" />
+                <Button onClick={handleReset} text="Reset" />
+              </>
+            ) : (
+              <>
+                <Button onClick={handleStartStop} text="Start" />
+                <Button onClick={handleReset} text="Reset" />
+              </>
+            )}
+          </>
+        )}
+        <audio ref={audioRef} loop autoPlay>
+          <source src="/cat-meows.mp3" type="audio/mpeg" />
+        </audio>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
