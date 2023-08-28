1faa767cb6ba0a4c9c5bf2414c9d650174aa069c
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-08-27 22:05:50 -0700

    Add stopwatch component
    Added logic to handle components selection.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-import Timer from './components/Timer';
+import React, { useState } from "react";
+import "./App.css";
+import Timer from "./components/Timer";
+import Stopwatch from "./components/Stopwatch";
 
 function App() {
+  const [selectedComponent, setSelectedComponent] = useState(null);
+
+  const handleComponentSelection = (component) => {
+    setSelectedComponent(component);
+  };
+
   return (
     <div className="App">
-      <Timer />
+      <div className="component-selection">
+        <button onClick={() => handleComponentSelection("timer")}>Timer</button>
+        <button onClick={() => handleComponentSelection("stopwatch")}>Stopwatch</button>
+      </div>
+      {selectedComponent === "timer" && <Timer />}
+      {selectedComponent === "stopwatch" && <Stopwatch />}
     </div>
   );
 }
