b0e025931a7d1a594a57e501d965483147fe7c21
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-09-05 00:43:39 -0700

    Write tests for Stopwatch component

---
+++ src/components/__tests__/Stopwatch.test.js
@@ -0,0 +1,68 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
+import { render, fireEvent, act } from "@testing-library/react";
+import Stopwatch from "../Stopwatch";
+
+// Helper function to advance time in the component
+// const advanceTime = (time) => jest.advanceTimersByTime(time);
+
+
+
+describe("Stopwatch", () => {
+  beforeEach(() => {
+    jest.useFakeTimers(); // Mock timers
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  it("starts and stops the stopwatch when clicking start/stop button", () => {
+    const { getByText } = render(<Stopwatch />);
+
+    expect(getByText("00s 00")).toBeInTheDocument();
+
+    const startButton = getByText("Start");
+    fireEvent.click(startButton);
+
+    const stopButton = getByText("Stop");
+    expect(stopButton).toBeInTheDocument(); // Stop button should appear
+
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+
+    act(() => {
+     jest.runOnlyPendingTimers();
+    });
+
+    expect(getByText("01s 00")).toBeInTheDocument();
+
+    fireEvent.click(stopButton);
+
+    expect(startButton).toBeInTheDocument(); // Start button should reappear
+  });
+
+  it("resets the stopwatch when clicking reset button", () => {
+    const { getByText } = render(<Stopwatch />);
+
+    const startButton = getByText("Start");
+    fireEvent.click(startButton);
+
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+
+    
+    const resetButton = getByText("Reset");
+    fireEvent.click(resetButton);
+    
+    expect(startButton).toBeInTheDocument();
+    
+    expect(getByText("00s 00")).toBeInTheDocument();
+  });
+});
