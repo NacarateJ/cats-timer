4888fb8b727ce029af36b5400021f20e75171f74
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-09-05 14:45:57 -0700

    Refactor code
    While running my tests I noticed that my stopwatch had a delay if compared to my phone's stopwatch. I had to change the way I was formating the time in order to make it work properly. Refactoring also made the code cleaner and easier to understand.

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,10 +2,7 @@ import React, { useState, useEffect } from "react";
 import Button from "./Button";
 
 const Stopwatch = () => {
-  const [milliseconds, setMilliseconds] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
+  const [time, setTime] = useState(0)
   const [isTimerRunning, setIsTimerRunning] = useState(false);
 
   /**
@@ -16,50 +13,32 @@ const Stopwatch = () => {
 
     if (isTimerRunning) {
       intervalId = setInterval(() => {
-        setMilliseconds((prevMilliseconds) => prevMilliseconds + 10);
-
-        if (milliseconds >= 1000) {
-          setMilliseconds(0);
-          setSeconds((prevSeconds) => prevSeconds + 1);
-        }
-
-        if (seconds >= 60) {
-          setSeconds(0);
-          setMinutes((prevMinutes) => prevMinutes + 1);
-        }
-
-        if (minutes >= 60) {
-          setMinutes(0);
-          setHours((prevHours) => prevHours + 1);
-        }
+        setTime((prevTime) => prevTime +10);
       }, 10);
     } else {
       clearInterval(intervalId);
     }
 
     return () => clearInterval(intervalId);
-  }, [isTimerRunning, milliseconds, seconds, minutes]);
+  }, [isTimerRunning]);
 
   /**
    * Format the time in hours, minutes, seconds, and milliseconds.
    * @returns {string} - Formatted time string.
    */
   const formatTime = () => {
-    const formattedHours = hours.toString().padStart(1, "0");
-    const formattedMinutes = minutes.toString().padStart(2, "0");
-    const formattedSeconds = seconds.toString().padStart(2, "0");
-    const formattedMilliseconds = milliseconds
-      .toString()
-      .padStart(3, "0")
-      .slice(0, 2);
+   const formattedHours = ("0" + Math.floor((time / 3600000) % 24)).slice(-2);
+   const formattedMinutes = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
+   const formattedSeconds = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
+   const formattedMilliseconds = ("00" + (time % 1000)).slice(-2);
 
-    if (hours > 0) {
-      return `${formattedHours}h ${formattedMinutes}m ${formattedSeconds}s ${formattedMilliseconds}`;
-    } else if (minutes > 0) {
-      return `${formattedMinutes}m ${formattedSeconds}s ${formattedMilliseconds}`;
-    } else {
-      return `${formattedSeconds}s ${formattedMilliseconds}`;
-    }
+   if (formattedHours !== "00") {
+     return `${formattedHours}h ${formattedMinutes}m ${formattedSeconds}s ${formattedMilliseconds}`;
+   } else if (formattedMinutes !== "00") {
+     return `${formattedMinutes}m ${formattedSeconds}s ${formattedMilliseconds}`;
+   } else {
+     return `${formattedSeconds}s ${formattedMilliseconds}`;
+   }
   };
 
   /**
@@ -74,10 +53,7 @@ const Stopwatch = () => {
    */
   const handleReset = () => {
     setIsTimerRunning(false);
-    setMilliseconds(0);
-    setSeconds(0);
-    setMinutes(0);
-    setHours(0);
+    setTime(0);
   };
 
   return (
