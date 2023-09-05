c92408b5ddb45f59ffdf87271c7086cefad5cf12
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-09-05 00:42:45 -0700

    Write tests for App component

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
+import {
+  render,
+  fireEvent,
+} from "@testing-library/react";
+
+import App from "../../App";
+
+test("renders App component", () => {
+  const { getByText } = render(<App />);
+  const timerButton = getByText("Timer");
+  const stopwatchButton = getByText("Stopwatch");
+
+  expect(timerButton).toBeInTheDocument();
+  expect(stopwatchButton).toBeInTheDocument();
+});
+
+test("renders Timer component when Timer button is clicked", () => {
+  const { getByText, queryByText } = render(<App />);
+  const timerButton = getByText("Timer");
+
+  fireEvent.click(timerButton);
+
+  expect(getByText("00:05:00")).toBeInTheDocument(); 
+  expect(queryByText("00s 00")).not.toBeInTheDocument();
+});
+
+test("renders Stopwatch component when Stopwatch button is clicked", () => {
+  const { getByText, queryByText } = render(<App />);
+  const stopwatchButton = getByText("Stopwatch");
+
+  fireEvent.click(stopwatchButton);
+
+  expect(queryByText("00:05:00")).not.toBeInTheDocument();
+  expect(getByText("00s 00")).toBeInTheDocument();
+});
