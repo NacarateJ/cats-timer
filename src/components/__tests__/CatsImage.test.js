d4cd0b96631b1b1754780c0a5089a688c4b96bf7
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-09-07 17:28:13 -0700

    Write tests for CatsImage component

---
+++ src/components/__tests__/CatsImage.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import axios from "axios";
+import CatsImage from "../CatsImage";
+
+describe("CatsImage", () => {
+  it("fetches and displays a random cat image", async () => {
+    const { getByAltText } = render(<CatsImage />);
+
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1)); // Adjust the number of expected calls based on your component
+
+    const catImage = getByAltText("Random Cat");
+    expect(catImage).toBeInTheDocument();
+  });
+});
