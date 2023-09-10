bda742d42467d52569d2b3708785fea14697183b
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-09-10 08:11:31 -0700

    Fix scss file path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import "./App.scss";
 import CatsImage from "./components/CatsImage";
 import Timer from "./components/Timer";
 import Stopwatch from "./components/Stopwatch";
